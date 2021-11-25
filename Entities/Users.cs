a443509dfad21ab62065868238418816221e87a1
Your Name <you@example.com>
2021-11-25 16:41:05 +0000

    Add Dto, modify Users entity

--- Entities/Users.cs
+++ Entities/Users.cs
@@ -11,14 +11,27 @@ namespace YJKBooks.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        //A primary key constrain is a column that uniquely identifies every row in the table of the relational database management system
         public int Id { get; set; }
 
         [Required(ErrorMessage = "You should provide a name value.")]
         [MaxLength(50)]
         public string Name { get; set; }
 
-        [MaxLength(200)]
-        public string Description { get; set; }
+        [Required(ErrorMessage = "You should provide a valid email")]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You should provide a valid address.")]
+        public string Address { get; set; }
+
+        [ForeignKey("CityId")]
+        //navigation properties( navigate the point of interest to the parent city
+        public Book favouritebooks { get; set; }
+        public int BookId { get; set; }
+
+        //A primary key constrain is a column that uniquely identifies every row in the table of the relational database management system, 
+        //while foreign key is a column that creates a relationship between two tables.
+
+
     }
 }
---
+++ Models/UsersDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YJKBooks.Models
+{
+    public class UsersDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public ICollection<BookDto> favouritebooks { get; set; } = new List<BookDto>();
+
+    }
+}
+
