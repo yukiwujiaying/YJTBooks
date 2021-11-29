fa02e4de20617ca528f5ddca5a1a9f7d732a9c0c
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-11-29 12:19:56 +0000

    Commented out a Https_Redirection from Startup class

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace YJKBooks
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
