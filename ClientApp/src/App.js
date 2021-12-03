1996f409567a721cd5f4655d68043e0edbe16804
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-03 15:20:08 +0000

    Deleted a line in App.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,7 +4,6 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
 import './custom.css'
 
 export default class App extends Component {
