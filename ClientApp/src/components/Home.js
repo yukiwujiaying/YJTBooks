3c22f081361d0bd55f93aa8e71ec92336e5c4373
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-07 10:25:09 +0000

    Deleted default test on Home page

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+    const products = [
+        {name: 'prod1', price: 10.99 },
+        {name: 'prod2', price: 11.99 },
+    ]
 
   render () {
     return (
       <div>
-        <h1>Hello, Tedy!</h1> 
+            <h1>Hello, Tedy!</h1>
+            <ul>
+                {products.map(item => (
+                    <li>{item.name} - {item.price}</li>
+                ))}
+}
+            </ul>
         <p>Welcome to you next coding adventure</p>
       
         <p>To help you get started, we have also set up:</p>
