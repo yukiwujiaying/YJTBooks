892a341036ce882f11cd55c0b44815914b69eb19
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-09 17:12:52 +0000

    Modified Home to be a function component, added a useEffect hook to fetch data from DB, data is printed on port 5000 only atm

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 
-export class Home extends Component {
-    static displayName = Home.name;
-   /* const products = [
-        {name: 'prod1', price: 10.99 },
-        {name: 'prod2', price: 11.99 },
-    ]
-    */
-  render () {
+export function Home() {
+    // const { id } = useParams()
+    const [books, setBooks] = useState([]);
+    // const url = `http://localhost:5000/api/books/${id}`
+
+    useEffect(() => {
+
+        //this function fetches our books from the API
+        fetch('http://localhost:5000/api/books')
+            //the fetch returns a promise(response) which we turn into  a json object 
+            .then(response => response.json())
+            .then(data => setBooks(data))
+        //we use the empty array dependency [] which makes sure that we only call useEffect() once. 
+    }, [])
+
+
     return (
-      <div>
-            <h1>Hello, Tedy!</h1>
-           
-        <p>Welcome to you next coding adventure</p>
-      
-            
-      </div>
+        <div className='home'>
+
+            <ul>
+                {books.map((item, index) => (
+                    <li key={index}>{item.bookId} - {item.title} - {item.author} - {item.price} - {item.amazonLink} - {item.synopsis} - {item.pictureUrl}</li>
+                ))}
+            </ul>
+
+        </div>
     );
-  }
 }
