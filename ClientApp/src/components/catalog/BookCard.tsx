269555ebf452c0fd08eb23659e557109e74721da
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-13 19:03:31 +0000

    Added th BookList and BookCard components

---
+++ ClientApp/src/components/catalog/BookCard.tsx
@@ -0,0 +1,23 @@
+﻿import { ListItem, ListItemAvatar, ListItemText, List } from '@material-ui/core';
+import { Avatar } from '@mui/material';
+import React, { Component } from 'react';
+import Book from '../../app/models/book';
+
+interface Props {
+    //The property is called books, and it's of type Book
+    books: Book;
+}
+
+export default function BookCard({ books }: Props) {
+    return (
+                <ListItem key={books.bookId}>
+                    <ListItemAvatar>
+                        <Avatar src={books.pictureUrl} />
+                    </ListItemAvatar>
+
+                    <ListItemText>
+                        {books.title} - {books.author} - {books.price}
+                    </ListItemText >
+                </ListItem>
+          )
+}
---
+++ ClientApp/src/components/catalog/BookList.tsx
@@ -0,0 +1,19 @@
+﻿import { List } from '@material-ui/core';
+import Book from '../../app/models/book';
+import React, { Component } from 'react';
+import BookCard from "./BookCard";
+
+interface Props {
+    books: Book[];
+}
+
+export default function BookList({ books } : Props) {
+
+    return (
+        <List>
+            {books.map(book => (
+                <BookCard key={book.bookId} books = {book} />
+            ))}
+        </List>
+    )
+}
