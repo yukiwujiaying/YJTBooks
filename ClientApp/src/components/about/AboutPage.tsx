e4ed6624bc4735dc9ada67bf617c9ca9f59cdd6d
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-15 11:14:44 +0000

    Created the about, contact pages,and home pages

---
+++ ClientApp/src/components/about/AboutPage.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import { Typography } from "@mui/material";
+
+export default function AboutPage(){
+    return(
+        <Typography variant='h2'>
+            About Page
+        </Typography>
+    )
+}
---
+++ ClientApp/src/components/home/HomePage.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import { Typography } from "@mui/material";
+
+export default function HomePage() {
+    return (
+        <Typography variant='h2'>
+            Home Page
+        </Typography>
+    )
+}
