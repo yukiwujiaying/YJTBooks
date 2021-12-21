de87f02d3101a37fa3652af90a2ef37abd8c7af3
-loper-tpavlova1 <-loper-tpavlova1@uk.kworld.kpmg.com>
2021-12-21 18:45:23 +0000

    Created a configureStore and accountSlice

---
+++ ClientApp/src/app/store/configureStore.ts
@@ -0,0 +1,20 @@
+﻿import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { accountSlice } from "../../components/Account/accountSlice";
+// import { catalogSlice } from "../../components/catalog/catalogSlice";
+
+// export function configureStore() {
+//     return createStore(counterReducer);
+// }
+
+export const store = configureStore({
+    reducer: {
+        account: accountSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- ClientApp/src/components/Account/accountSlice.ts
+++ ClientApp/src/components/Account/accountSlice.ts
@@ -1,4 +1,8 @@
 ﻿import { User } from "../../app/models/user";
+import { createSlice, createAsyncThunk, isAnyOf } from "@reduxjs/toolkit";
+import { FieldValues } from "react-hook-form";
+import agent from "../../app/api/agent";
+import { myHistory } from "../../history";
 
 
 interface AccountState {
@@ -9,3 +13,51 @@ const initialState: AccountState = {
     user: null
 }
 
+export const signInUser = createAsyncThunk<User, FieldValues>(
+    'account/signInUser',
+    async (data, thunkAPI) => {
+        try {
+            const user = await agent.Account.login(data);
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
+        }
+    }
+)
+
+export const fetchCurrentUser = createAsyncThunk<User>(
+    'account/signInUser',
+    async (_, thunkAPI) => {
+        try {
+            const user = await agent.Account.currentUser();
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
+        }
+    }
+)
+
+export const accountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+        signOut: (state) => {
+            state.user = null;
+            localStorage.removeItem('user');
+            myHistory.push('/');
+        },
+    },
+    extraReducers: (
+        builder => {
+            builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
+                state.user = action.payload;
+            });
+            builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
+                console.log(action.payload);
+            })
+      })
+})
+
+export const { signOut } = accountSlice.actions;
