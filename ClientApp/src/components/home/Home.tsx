07bb00b0385a936a132d1c128b6b151b08e21111
ywu11 <yuki.wu@kpmg.co.uk>
2022-01-20 19:30:48 +0000

    Seach page

--- ClientApp/src/components/home/Home.tsx
+++ ClientApp/src/components/home/Home.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
-import BookSearch from '../catalog/BookSearch';
+import { Paper, IconButton, InputBase, Divider, debounce } from '@mui/material';
+import SearchIcon from '@material-ui/icons/Search';
+import React, { useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../app/store/configureStore';
+import { setBookParams } from '../catalog/catalogSlice';
+import { Link } from 'react-router-dom';
 
-export default function Home()  {
-    
+
+export default function Home() {
+    const { bookParams } = useAppSelector(state => state.catalog);
+    const [searchTerm, setSearchTerm] = useState(bookParams.searchTerm);
+    const dispatch = useAppDispatch();
+   
+
+    const debouncedSearch = debounce((event: any) => {
+        dispatch(setBookParams({ searchTerm: event.target.value }))
+    }, 1500)
+
     return (
-        <BookSearch /> 
+        <Paper
+            component="form"
+            className="searchBox"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+        >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search Books"
+                value={searchTerm || ''}
+                onChange={(event: any) => {
+                    setSearchTerm(event.target.value);
+                    debouncedSearch(event);
+                }}
+            />
+            <IconButton 
+                type="submit" sx={{ p: '10px' }} 
+                aria-label="search" 
+                disabled={!searchTerm}
+                component={Link} to={`/catalog`}
+                >
+                <SearchIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        </Paper>
     )
 }
